793059d77c807fe8b9110a6677889804c0161281
Shinichiro Hamaji <shinichiro.hamaji _at_ gmail.com>
2009-07-06 09:22:48 +0900

    divmod

---
+++ divmod.c
@@ -0,0 +1,4 @@
+int main() {
+    printf("%d\n", -1 / 4);
+    printf("%d\n", -1 % 4);
+}
---
+++ divmod.java
@@ -0,0 +1,6 @@
+public class divmod {
+    public static void main(String[] args) {
+        System.out.println(-1 / 4);
+        System.out.println(-1 % 4);
+    }
+}
