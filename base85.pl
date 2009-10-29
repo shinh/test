8ba89304cc8a3970a4ec57523ccbff350ca55ecd
Shinichiro Hamaji <shinichiro.hamaji _at_ gmail.com>
2009-10-30 07:55:10 +0900

    base85 decoder

---
+++ base85.pl
@@ -0,0 +1,39 @@
+use Compress::Zlib;
+
+# http://tools.ietf.org/html/rfc1924
+sub decodeBase85($)
+{
+    my ($encoded) = @_;
+    my %table;
+    @characters = ('0'..'9', 'A'..'Z', 'a'..'z', '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', ';', '<', '=', '>', '?', '@', '^', '_', '`', '{', '|', '}', '~');
+    for (my $i = 0; $i < 85; $i++) {
+        $table{$characters[$i]} = $i;
+    }
+
+    my $decoded = '';
+    my @encodedChars = $encoded =~ /./g;
+
+    #print @encodedChars+0, $/;
+
+    for (my $encodedIter = 0; $encodedChars[$encodedIter];) {
+        my $digit = 0;
+        for (my $i = 0; $i < 5; $i++) {
+            $digit *= 85;
+            my $char = $encodedChars[$encodedIter];
+            $digit += $table{$char};
+            $encodedIter++;
+        }
+
+        for (my $i = 0; $i < 4; $i++) {
+            $decoded .= chr(($digit >> (3 - $i) * 8) & 255);
+        }
+    }
+
+    #return $decoded;
+    return uncompress($decoded);
+}
+
+
+print decodeBase85('cmcgoK@Nm42&C%Gi}VGyC*RS#Puc$;>j1^7i6@ht1lnN+#Fo7bq$tQ6Vr>y<!VQ4_
+n=M9AiCU{Da<TJsX5cb}GtdhmC+3(JcSKMWQsW^DWp)E(?RP^ICa{KJM6ym)p49Zt
+t*7gEj&Ea!K94a|!ZwboL2nB`_MvkjvlBIg|DPLNCVIRN3V^@Pf6oJ;{2S~5')
