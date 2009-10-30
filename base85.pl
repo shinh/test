f8ff7b95b9cb68fb34d0896d31a6b96103da1324
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2009-10-30 17:03:01 +0900

    hogehoge

--- base85.pl
+++ base85.pl
@@ -37,4 +37,6 @@ sub decodeBase85($)
 #print decodeBase85('cmcgoK@Nm42&C%Gi}VGyC*RS#Puc$;>j1^7i6@ht1lnN+#Fo7bq$tQ6Vr>y<!VQ4_
 #n=M9AiCU{Da<TJsX5cb}GtdhmC+3(JcSKMWQsW^DWp)E(?RP^ICa{KJM6ym)p49Zt
 #t*7gEj&Ea!K94a|!ZwboL2nB`_MvkjvlBIg|DPLNCVIRN3V^@Pf6oJ;{2S~5')
-print decodeBase85('cmaDfkMYtx#tk1lCRcfiOy==S+g#=Of)xOJZVC1P')
+
+print decodeBase85('cmZ3>wTWv3Hw#N{etPOgM>$5;Q#<yZ=K=s#!w3!l')
+#print decodeBase85('cmaDfkMYtx#tk1lCRcfiOy==S+g#=Of)xOJZVC1P')
