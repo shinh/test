a85da487024755cfc8d2d5b8f718dbc8cfd7ba5f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2009-11-14 13:06:05 +0900

    add reference

--- go/464.go
+++ go/464.go
@@ -1,4 +1,7 @@
 func main() {
 	// stupid ha omae dayo
 	i := 1 << 464;
+	// src/cmd/gc/go.h
+	// Mpscale = 29,       // safely smaller than bits in a long
+	// Mpprec  = 16,       // Mpscale*Mpprec is max number of bits
 }
