2ede6643868bea98c394c5df2a31ea781c528cc9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2009-12-01 07:30:24 +0900

    GOMAXPROCS

--- go/goroutine.go
+++ go/goroutine.go
@@ -1,15 +1,20 @@
 package main
+
+import "runtime"
+
 func f(c chan int, i int) {
 	//println(i);
 	c <- i
 }
 func main() {
+	runtime.GOMAXPROCS(4);
+
 	c := make(chan int, 2);
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 100000; i++ {
 		go f(c, i)
 	}
 	s := 0;
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 100000; i++ {
 		j := <- c;
 		//println(j);
 		s += j;
