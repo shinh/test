b9b3e1c88bb9912e8ade95d4f664916739e4c90a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2010-01-10 22:25:25 +0900

    display the type in string

--- readdir.c
+++ readdir.c
@@ -1,10 +1,34 @@
 #include <dirent.h>
 #include <stdio.h>
 
+const char* type_str(unsigned char type) {
+    switch (type) {
+    case DT_BLK:
+        return "block device";
+    case DT_CHR:
+        return "character device";
+    case DT_DIR:
+        return "directory";
+    case DT_FIFO:
+        return "fifo";
+    case DT_LNK:
+        return "symlink";
+    case DT_REG:
+        return "regular file";
+    case DT_SOCK:
+        return "socket";
+    case DT_UNKNOWN:
+        return "unknown";
+    default:
+        return "???";
+    }
+}
+
 int main() {
     DIR* dir = opendir(".");
     struct dirent* ent;
     while ((ent = readdir(dir)) != NULL) {
-        printf("%s %d\n", ent->d_name, ent->d_type);
+        printf("%30s\t%s(%d)\n",
+               ent->d_name, type_str(ent->d_type), ent->d_type);
     }
 }
