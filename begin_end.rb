c8a2eb9f83f8780caf1daf3536fda521abf2a3c0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2010-02-13 03:50:01 +0900

    invert version

--- begin_end.rb
+++ begin_end.rb
@@ -16,3 +16,24 @@ BEGIN {
     }
   }
 }
+
+__END__
+
+END {
+  print "wor";
+  BEGIN {
+    print "o, ";
+    END {
+      print "ld";
+      BEGIN {
+        print "ll";
+        END {
+          print "!\n";
+          BEGIN {
+            print "He";
+          }
+        }
+      }
+    }
+  }
+}
