39195254c464dd4393937b9d78915344f48c15aa
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2012-04-10 01:00:42 +0900

    Update stdarg.c

--- stdarg.c
+++ stdarg.c
@@ -1,6 +1,16 @@
-void f(int i, ...) {
+#include <stdarg.h>
 
+void f(int i, ...) {
+  va_list ap;
+  va_start(ap, i);
+  for (i = 0; i < 10; i++) {
+    double v = va_arg(ap, double
+
+);
+  }
+  va_end(ap);
 }
 
 int main() {
+  puts("hello-");
 }
