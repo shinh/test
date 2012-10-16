ac6282f23cdc93fd00b9162d78bee00c3af61254
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2012-10-16 21:34:10 +0900

    A C code which takes a lot of time without using much RAM
    It takes 100 secs to build using 100MB RAM

---
+++ long_run.c
@@ -0,0 +1,26 @@
+#ifndef A
+#define A B B
+#define B C C
+#define C D D
+#define D E E
+#define E F F
+#define F G G
+#define G H H
+#define H I I
+#define I J J
+#define J K K
+#define K L L
+#define L M M
+#define M N N
+#define N O O
+#define O P P
+#define P Q Q
+#define Q R R
+#define R S S
+#define S T T
+#define T U U
+#define U V V
+#define V ;
+#endif
+A
+#include __FILE__
