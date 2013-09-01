9377476ab1ce129ac7d878fc2a14491b62a43281
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2013-09-01 14:14:19 +0900

    Show count of RTM based locking

--- deadlock.cc
+++ deadlock.cc
@@ -1,4 +1,7 @@
+#include <immintrin.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdint.h>
 
 #define NTHREAD 2
 
@@ -6,17 +9,21 @@ pthread_mutex_t mu1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mu2 = PTHREAD_MUTEX_INITIALIZER;
 
 void* thread_func(void* arg) {
+  intptr_t tsx_cnt = 0;
   for (int i = 0; i < 100; i++) {
     pthread_mutex_lock(&mu1);
     pthread_mutex_lock(&mu2);
+    tsx_cnt += _xtest();
     pthread_mutex_unlock(&mu2);
     pthread_mutex_unlock(&mu1);
 
     pthread_mutex_lock(&mu2);
     pthread_mutex_lock(&mu1);
+    tsx_cnt += _xtest();
     pthread_mutex_unlock(&mu1);
     pthread_mutex_unlock(&mu2);
   }
+  return (void*)tsx_cnt;
 }
 
 int main() {
@@ -25,6 +32,8 @@ int main() {
     pthread_create(&th[i], NULL, &thread_func, NULL);
   }
   for (int i = 0; i < NTHREAD; i++) {
-    pthread_join(th[i], NULL);
+    void* tsx_cnt;
+    pthread_join(th[i], &tsx_cnt);
+    printf("%d\n", (int)(intptr_t)tsx_cnt);
   }
 }
