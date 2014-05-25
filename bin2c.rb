bb8a0d38eb2f62e898a61ddf9de04ff0a1068abf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2014-05-25 18:03:37 +0900

    Make bin2c.rb faster

--- bin2c.rb
+++ bin2c.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-out = %Q[#include <unistd.h>
+print %Q[#include <unistd.h>
 int main(){write(1,"]
 
 prev_escape = false
@@ -8,20 +8,18 @@ content = File.read(ARGV[0])
 content.each_byte{|b|
   if b >= 32 && b < 127 && b != ?".ord && b != ?\\.ord
     if b.chr =~ /[0-9a-fA-F]/ && prev_escape
-      out += "\\x%02x" % b
+      print "\\x%02x" % b
       prev_escape = true
     else
-      out += "%c" % b
+      print "%c" % b
       prev_escape = false
     end
   elsif b == 0
-    out += "\\0"
+    print "\\0"
     prev_escape = true
   else
-    out += "\\x%02x" % b
+    print "\\x%02x" % b
     prev_escape = true
   end
 }
-out += %Q[",#{content.size});}]
-
-puts out
+puts %Q[",#{content.size});}]
