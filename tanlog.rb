45e5b303b3b78edf8e89da4ee2571e125eaf412c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-05-09 15:18:48 +0900

    Update the sample .zshrc snippet

--- tanlog.rb
+++ tanlog.rb
@@ -1,47 +1,44 @@
 #!/usr/bin/env ruby
 #
 # inspired by https://github.com/omakoto/zenlog
-#
-# _maybe_screen() {
-#     if [ "x$TERM" = "xscreen" ]; then
-#         if [ "x$SSH_TTY" = "x" ]; then
-#             screen "$@"
-#         fi
-#     fi
-# }
-#
-# tanlog_begin() {
-#     local date=$(date '+%Y-%m-%d')
-#     local logdir="/tmp/tanlog/${date}"
-#     if [ ! -d $logdir ]; then
-#         mkdir -p $logdir
-#     fi
-#     local time=$(date '+%H:%M:%S')
-#     local n=0
-#     local logfile=""
-#     while [ -z $logfile -o -e $logfile ]; do
-#         logfile="${logdir}/${time}-${n}-raw.log"
-#         n=$(($n+1))
-#     done
-#     echo "\$ $1" > $logfile
-#
-#     export TANLOG_LOGFILE=$logfile
-#     _maybe_screen -X logfile $logfile
-#     _maybe_screen -X log on
-# }
-#
-# tanlog_end() {
-#     _maybe_screen -X log off
-#     $HOME/test/tanlog.rb $TANLOG_LOGFILE &
-# }
-#
-# preexec() {
-#     tanlog_begin
-# }
-#
-# precmd() {
-#     tanlog_end
-# }
+
+ZSH_CODE = <<EOC
+_maybe_screen() {
+    if [[ "$TERM" =~ "screen.*" ]]; then
+        if [ "x$SSH_TTY" = "x" ]; then
+            screen "$@"
+        fi
+    fi
+}
+
+tanlog_begin() {
+    local date=$(date '+%Y-%m-%d')
+    local logdir="/tmp/tanlog/${date}"
+    if [ ! -d $logdir ]; then
+        mkdir -p $logdir
+    fi
+    local time=$(date '+%H:%M:%S')
+    local n=0
+    local logfile=""
+    while [ -z "$logfile" -o -e $logfile ]; do
+        logfile="${logdir}/${time}-${n}-raw.log"
+        n=$(($n+1))
+    done
+    echo "\$ $1" > $logfile
+
+    export TANLOG_LOGFILE=$logfile
+    _maybe_screen -X logfile $logfile
+    _maybe_screen -X log on
+}
+
+tanlog_end() {
+    _maybe_screen -X log off
+    $HOME/test/tanlog.rb $TANLOG_LOGFILE &!
+}
+
+preexec_functions+=tanlog_begin
+precmd_functions+=tanlog_end
+EOC
 
 def rotate_log
   # TODO
