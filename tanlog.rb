ec983fe2210cdcb0ab0181a7d92d327754a0eeb2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-06-02 14:41:28 +0900

    Remove old link beofre creating a new one

--- tanlog.rb
+++ tanlog.rb
@@ -56,6 +56,9 @@ def setup_cmd_link(logfile, cmd)
      [raw_to_san(cmddir), raw_to_san(logfile)]].each do |cd, lf|
       FileUtils.mkdir_p(cd)
       dest = "#{cd}/#{File.basename(lf)}"
+      if File.exist?(dest)
+        File.unlink(dest)
+      end
       FileUtils.ln_s(lf, dest)
 
       create_prev_links(lf, cd)
