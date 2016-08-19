5723530932e2ca08e72b75dafedc41b4a03e9465
hamaji <shinichiro.hamaji@gmail.com>
2016-08-19 11:23:29 +0900

    Make it possible to attach a process

--- tracer.cc
+++ tracer.cc
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ptrace.h>
+#include <sys/user.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -9,30 +12,15 @@
 #define PT_STEP PTRACE_SINGLESTEP
 #endif
 
-typedef struct {
 #ifdef __x86_64__
-  #define BP rbp
-  #define SP rsp
-  #define IP rip
-  uint64_t  r15,r14,r13,r12,rbp,rbx,r11,r10;
-  uint64_t  r9,r8,rax,rcx,rdx,rsi,rdi,orig_rax;
-  uint64_t  rip,cs,eflags;
-  uint64_t  rsp,ss;
-  uint64_t  fs_base, gs_base;
-  uint64_t  ds,es,fs,gs;
+  #define BP regs.rbp
+  #define SP regs.rsp
+  #define IP regs.rip
 #else
-  #define BP ebp
-  #define SP esp
-  #define IP eip
-  uint32_t  ebx, ecx, edx, esi, edi, ebp, eax;
-  uint16_t  ds, __ds, es, __es;
-  uint16_t  fs, __fs, gs, __gs;
-  uint32_t  orig_eax, eip;
-  uint16_t  cs, __cs;
-  uint32_t  eflags, esp;
-  uint16_t  ss, __ss;
+  #define BP regs.ebp
+  #define SP regs.esp
+  #define IP regs.eip
 #endif
-} Regs;
 
 long checked_ptrace(enum __ptrace_request req, pid_t pid, long addr, long data,
                     const char* req_str, const char* file, int line) {
@@ -51,25 +39,33 @@ long checked_ptrace(enum __ptrace_request req, pid_t pid, long addr, long data,
 int main(int argc, char* argv[]) {
   argc--;
   argv++;
-  pid_t pid = fork();
-  if (pid) {
-    long prev = 0;
-    while (true) {
-      int status;
-      wait(&status);
-      if (!WIFSTOPPED(status))
-        break;
 
-      Regs regs;
-      CHECKED_PTRACE(PTRACE_GETREGS, pid, 0, (long)&regs);
-      if (prev != regs.IP)
-        printf("%p\n", regs.IP);
-      prev = regs.IP;
-
-      CHECKED_PTRACE(PTRACE_SINGLESTEP, pid, 0, 0);
-    }
+  pid_t pid;
+  if (!strcmp(argv[0], "-p")) {
+    pid = strtol(argv[1], 0, 10);
+    CHECKED_PTRACE(PTRACE_ATTACH, pid, 0, 0);
   } else {
-    CHECKED_PTRACE(PTRACE_TRACEME, 0, 0, 0);
-    execv(argv[0], argv);
+    pid = fork();
+    if (!pid) {
+      CHECKED_PTRACE(PTRACE_TRACEME, 0, 0, 0);
+      execv(argv[0], argv);
+      abort();
+    }
+  }
+
+  long prev = 0;
+  while (true) {
+    int status;
+    wait(&status);
+    if (!WIFSTOPPED(status))
+      break;
+
+    struct user regs;
+    CHECKED_PTRACE(PTRACE_GETREGS, pid, 0, (long)&regs);
+    if (prev != regs.IP)
+      printf("%p\n", (void*)regs.IP);
+    prev = regs.IP;
+
+    CHECKED_PTRACE(PTRACE_SINGLESTEP, pid, 0, 0);
   }
 }
