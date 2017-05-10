fcdb17401e535b09d44d41ef622f5aed82ed5387
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2017-05-10 17:29:15 +0900

    Add faster fib

--- fib.rb
+++ fib.rb
@@ -1,5 +1,29 @@
-n = ARGV[0].to_i
-fib = [1,1]
-n.times{|i|
-  fib << fib[i+1] + fib[i]
-}
+def fib(n)
+  a=0
+  b=1
+  while n>0
+    n-=1
+    a,b=b,a+b
+  end
+  return a
+end
+
+def fib_fast(n)
+  atot=1
+  btot=0
+  dtot=1
+  a=1
+  b=1
+  d=0
+  while n>0
+    if n%2!=0
+      atot,btot,dtot=atot*a+btot*b,atot*b+btot*d,btot*b+dtot*d
+    end
+    n/=2
+    a,b,d=a*a+b*b,a*b+b*d,b*b+d*d
+  end
+  return btot
+end
+
+p fib_fast(1000000)
+p fib(1000000)
