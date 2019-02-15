ecd1a71fda31e21b47ab849de1dcb93bbeb4698a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-02-15 14:23:12 +0900

    Add reverse ops

--- py_operators.py
+++ py_operators.py
@@ -5,13 +5,25 @@ ops = set()
 for o in dir(operator):
     ops.add(o)
 
+oops = []
 for o in sorted(ops):
     if o.startswith('_'):
         continue
     if o.endswith('_'):
         o = o[:-1]
+    if o.startswith('i') and '__%s__' % o[1:] in ops:
+        continue
+
     o = '__%s__' % o
     if o in ops:
-        print('    def %s(self, *args):' % o)
-        print("        return self.__getattr__('%s')(*args)" % o)
-        print('')
+        oops.append(o)
+
+oops.extend(
+    ['__radd__', '__rsub__', '__rmul__', '__rdiv__', '__rtruediv__',
+     '__rfloordiv__', '__rmod__', '__rdivmod__', '__rpow__', '__rlshift__',
+     '__rrshift__', '__rand__', '__rxor__', '__ror__'])
+
+for o in oops:
+    print('    def %s(self, *args):' % o)
+    print("        return self.__getattr__('%s')(*args)" % o)
+    print('')
