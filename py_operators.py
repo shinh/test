a00e3020dcb1bd7f077ae5560cc8188bd11ae883
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-02-15 14:12:32 +0900

    List up a list of Python operators

---
+++ py_operators.py
@@ -0,0 +1,17 @@
+import operator
+
+
+ops = set()
+for o in dir(operator):
+    ops.add(o)
+
+for o in ops:
+    if o.startswith('_'):
+        continue
+    if o.endswith('_'):
+        o = o[:-1]
+    o = '__%s__' % o
+    if o in ops:
+        print('    def %s(self, *args):' % o)
+        print("        return self.__getattr__('%s')(*args)" % o)
+        print('')
