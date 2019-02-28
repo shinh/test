01fe410355725fff7d25e0d7d6c8099e0ab8f602
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-03-01 03:28:42 +0900

    Update run_imagenet_ngraph for new API

--- run_imagenet_ngraph.py
+++ run_imagenet_ngraph.py
@@ -8,19 +8,19 @@ from ngraph_onnx.onnx_importer.importer import import_onnx_model
 
 model = onnx.load(sys.argv[1])
 
-ng_model = import_onnx_model(model)[0]
+ng_func = import_onnx_model(model)
 #print(ng_model)
 
 picture = np.ones([1, 3, 224, 224], dtype=np.float32)
 
 runtime = ng.runtime(backend_name='CPU')
 #runtime = ng.runtime(backend_name='GPU')
-resnet = runtime.computation(ng_model['output'], *ng_model['inputs'])
+resnet = runtime.computation(ng_func)
 #print(resnet)
 
 def run():
   resnet(picture)
 
-n = 1000
+n = 100
 
 print(timeit.timeit('run()', globals=globals(), number=n) / n * 1000, 'msec')
