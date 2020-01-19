ccd4dce0c463e92a1739f47d30c3197027facbfc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2020-01-19 13:33:51 +0900

    Test print and sleep

---
+++ PrintSleep.java
@@ -0,0 +1,6 @@
+public class PrintSleep {
+    public static void main(String[] args) throws Exception {
+        System.out.print("hoge");
+        Thread.sleep(1000);
+    }
+}
---
+++ print_sleep.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    printf("hoge");
+    sleep(1);
+}
