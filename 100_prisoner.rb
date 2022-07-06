4cb02b443959331205174e517156c0efa6ae51f6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2022-07-06 13:07:53 +0900

    Search for the answer

--- 100_prisoner.rb
+++ 100_prisoner.rb
@@ -1,14 +1,17 @@
 N = 6
 
+$strat = []
+
+N.times do |i|
+  if i < N/2
+    $strat << [*0...N/2]
+  else
+    $strat << [*N/2...N]
+  end
+end
+
 def strat(i)
-  return [
-    [0,1,2],
-    [0,1,2],
-    [0,1,2],
-    [3,4,5],
-    [3,4,5],
-    [3,4,5],
-  ][i]
+  return $strat[i]
 
   r = []
   n = N/2
@@ -33,16 +36,53 @@ def check(perm)
   return true
 end
 
-cnt = 0
-ok_cnt = 0
-[*0...N].permutation.each do |perm|
-  ok = check(perm)
-  if ok
-    ok_cnt += 1
+
+10000.times do
+  cnt = 0
+  ok_cnt = 0
+  [*0...N].permutation.each do |perm|
+    ok = check(perm)
+    if ok
+      ok_cnt += 1
+    end
+    cnt += 1
+    # p [perm, ok]
   end
-  cnt += 1
-  p [perm, ok]
+
+  percent = ok_cnt.to_f * 100 / cnt
+  puts "#{ok_cnt}/#{cnt} #{percent}%"
+
+  #if percent > 5
+  #  raise
+  #end
+
+  tbl = []
+  loop {
+    cnts = {}
+    N.times do |i|
+      cnts[i] = N / 2
+    end
+
+    tbl = []
+    N.times do |i|
+      if cnts.size < N/2
+        break
+      end
+
+      s = []
+      (N/2).times do
+        r = rand(cnts.size)
+        v = cnts.keys[r]
+        cnts[v] -= 1
+        if cnts[v] == 0
+          cnts.delete(v)
+        end
+        s << v
+      end
+      tbl << s
+    end
+    break if tbl.size == N
+  }
+
+  $strat = tbl
 end
-
-puts "#{ok_cnt}/#{cnt} #{ok_cnt.to_f * 100 / cnt}%"
-
