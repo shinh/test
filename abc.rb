684dae2e7988d681c026d9fa49855e701a3df793
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2022-09-08 16:56:53 +0900

    Add abc.rb, failed entry for trick 2022

---
+++ abc.rb
@@ -0,0 +1,26 @@
+alias
+break
+class
+def /
+end
+freeze
+gets
+hash \
+in
+join
+Kernel
+loop {
+methods
+not
+object_id
+print
+Queue
+return
+super
+TRICK
+undef
+version }\
+while
+xyzzy =
+yield_self
+zzz... ZeroDivisionError
