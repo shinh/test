4cac5205167c14bb0ee82946a5c376cd3c176603
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2023-02-15 22:42:00 +0900

    Use secccomp

---
+++ use_seccomp.py
@@ -0,0 +1,20 @@
+import seccomp
+
+f = seccomp.SyscallFilter(defaction=seccomp.KILL)
+
+f.add_rule(seccomp.ALLOW, "read")
+f.add_rule(seccomp.ALLOW, "write")
+f.add_rule(seccomp.ALLOW, "rt_sigreturn")
+f.add_rule(seccomp.ALLOW, "rt_sigaction")
+f.add_rule(seccomp.ALLOW, "sigaltstack")
+f.add_rule(seccomp.ALLOW, "exit_group")
+
+log = open("log", "w")
+
+f.load()
+
+log.write("OK")
+log.flush()
+print("log writen", flush=True)
+
+malicious = open("malicious", "w")
