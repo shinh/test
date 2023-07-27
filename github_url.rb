23e6bc54aa24a526321269abb7220165329d23d6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2023-07-27 19:24:56 +0900

    Add github_url.rb

---
+++ github_url.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+file = ARGV[0]
+line = ARGV[1]
+
+file = File.realpath(file)
+git_dir = File.dirname(file)
+until File.exist?(git_dir + "/.git")
+  git_dir = File.dirname(git_dir)
+  if git_dir.size <= 1
+    raise "Not in git"
+  end
+end
+
+Dir.chdir(git_dir) do
+  url = `git remote get-url upstream 2> /dev/null`
+  if url.empty?
+    url = `git remote get-url origin 2> /dev/null`
+  end
+  if url.empty?
+    raise "Not in git"
+  end
+
+  url.gsub!(/git@(.*)?:/, 'https://\1/')
+  url.gsub!(/\.git$/, '')
+  url.chomp!
+
+  branches = `git branch --list`
+  branch = branches[/\s*(master|main|dev)$/, 1]
+
+  rel = file[git_dir.size..-1]
+
+  url = "#{url}/tree/#{branch}#{rel}"
+
+  if line
+    url += "#L#{line}"
+  end
+
+  puts url
+end
