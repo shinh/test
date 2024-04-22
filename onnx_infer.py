b48ac80f56399dc76117c73c0c5b78fe9f46667b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2024-04-22 09:22:37 +0900

    Fix usage of onnx.load

--- onnx_infer.py
+++ onnx_infer.py
@@ -5,6 +5,6 @@ import onnx.shape_inference
 if len(sys.argv) != 3:
     raise RuntimeError('Usage: %s input.onnx output.onnx' % sys.argv[0])
 
-model = onnx.load(sys.argv[1], 'rb')
+model = onnx.load(sys.argv[1])
 model = onnx.shape_inference.infer_shapes(model)
 onnx.save(model, sys.argv[2])
