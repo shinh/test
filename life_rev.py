a2f7822a9573d967f3c898163ffe8aa136129bb6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2024-07-10 10:20:38 +0900

    Take params in rev life

--- life_rev.py
+++ life_rev.py
@@ -1,3 +1,4 @@
+import sys
 from z3 import *
 
 
@@ -26,7 +27,7 @@ def step(w):
     return nw
 
 
-def z3_step(w):
+def z3_step(w, solver):
     H = len(w)
     W = len(w[0])
     nw = []
@@ -41,14 +42,15 @@ def z3_step(w):
                     cs.append(w[ny][nx])
             c = sum(cs)
             e = Or(c == 3, And(c == 4, w[y][x]))
+            # solver.add(c < 6)
             nr.append(e)
         nw.append(nr)
     return nw
 
 
-def z3_step_n(w, nsteps):
+def z3_step_n(w, nsteps, solver):
     for _ in range(nsteps):
-        w = z3_step(w)
+        w = z3_step(w, solver)
     return w
 
 
@@ -64,7 +66,7 @@ def rev_step(w, nsteps):
             row.append(Bool(f"cell_{x}_{y}"))
         cells.append(row)
 
-    ncells = z3_step_n(cells, nsteps)
+    ncells = z3_step_n(cells, nsteps, solver)
     for y in range(H):
         for x in range(W):
             e = ncells[y][x]
@@ -98,14 +100,15 @@ glider = parse(r"""
 """)
 
 
-def test_glider():
-    w = glider
+def test(w):
     for i in range(10):
         print(f"=== {i} ===")
         print("\n".join(w))
         w = step(w)
 
-# test_glider()
+# test(glider)
+
+test(parse(open(sys.argv[1]).read()))
 
 
 def test_rev(w, n):
@@ -127,12 +130,5 @@ test_rev(glider, 3)
 # ###.###
 # """))
 
-test_rev(parse(r"""
-.....................
-.####.....#....#...#.
-.#...#....#....##..#.
-.####.....#....#.#.#.
-.#...#....#....#..##.
-.####.....#....#...#.
-.....................
-"""), 2)
+
+test_rev(parse(open(sys.argv[1]).read()), int(sys.argv[2]))
