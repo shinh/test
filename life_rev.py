8488be9f4e5b376fd16bda70b2009fb8b6f8a0c3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2024-07-10 09:29:10 +0900

    Reverse game of life by z3

---
+++ life_rev.py
@@ -0,0 +1,138 @@
+from z3 import *
+
+
+def parse(s):
+    return s.strip().splitlines()
+
+
+def step(w):
+    H = len(w)
+    W = len(w[0])
+    nw = []
+    for y in range(H):
+        nr = ""
+        for x in range(W):
+            c = 0
+            for dy in range(-1, 2):
+                for dx in range(-1, 2):
+                    ny = (y + dy + H) % H
+                    nx = (x + dx + W) % W
+                    c += int(w[ny][nx] == "#")
+            if c == 3 or (c == 4 and w[y][x] == "#"):
+                nr += "#"
+            else:
+                nr += "."
+        nw.append(nr)
+    return nw
+
+
+def z3_step(w):
+    H = len(w)
+    W = len(w[0])
+    nw = []
+    for y in range(H):
+        nr = []
+        for x in range(W):
+            cs = []
+            for dy in range(-1, 2):
+                for dx in range(-1, 2):
+                    ny = (y + dy + H) % H
+                    nx = (x + dx + W) % W
+                    cs.append(w[ny][nx])
+            c = sum(cs)
+            e = Or(c == 3, And(c == 4, w[y][x]))
+            nr.append(e)
+        nw.append(nr)
+    return nw
+
+
+def z3_step_n(w, nsteps):
+    for _ in range(nsteps):
+        w = z3_step(w)
+    return w
+
+
+def rev_step(w, nsteps):
+    H = len(w)
+    W = len(w[0])
+    solver = Solver()
+
+    cells = []
+    for y in range(H):
+        row = []
+        for x in range(W):
+            row.append(Bool(f"cell_{x}_{y}"))
+        cells.append(row)
+
+    ncells = z3_step_n(cells, nsteps)
+    for y in range(H):
+        for x in range(W):
+            e = ncells[y][x]
+            if w[y][x] == "#":
+                solver.add(e)
+            else:
+                solver.add(Not(e))
+
+    res = solver.check()
+    assert res == sat
+
+    m = solver.model()
+    nw = []
+    for y in range(H):
+        nr = ""
+        for x in range(W):
+            if m[cells[y][x]]:
+                nr += "#"
+            else:
+                nr += "."
+        nw.append(nr)
+    return nw
+
+
+glider = parse(r"""
+........
+........
+...##...
+...#.#..
+...#....
+""")
+
+
+def test_glider():
+    w = glider
+    for i in range(10):
+        print(f"=== {i} ===")
+        print("\n".join(w))
+        w = step(w)
+
+# test_glider()
+
+
+def test_rev(w, n):
+    w = rev_step(w, n)
+    for i in range(n + 1):
+        print(f"=== {i} ===")
+        print("\n".join(w))
+        w = step(w)
+
+test_rev(glider, 3)
+
+# test_rev(parse(r"""
+# ###.###
+# #.#.#.#
+# ###.###
+# .......
+# ###.###
+# #.#.#.#
+# ###.###
+# """))
+
+test_rev(parse(r"""
+.....................
+.####.....#....#...#.
+.#...#....#....##..#.
+.####.....#....#.#.#.
+.#...#....#....#..##.
+.####.....#....#...#.
+.....................
+"""), 2)
